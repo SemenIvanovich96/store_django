0e8ea6618bb4abb03da4cf7692efffcfcc445e82
SemenIvanovich96 <SaiRex15@yandex.ru>
2026-05-08 10:39:11 +0300

    Добавлена валидация цены, исправлены шаблоны

--- store/forms.py
+++ store/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Product
 
 class ProductForm(forms.ModelForm):
@@ -6,12 +7,16 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'description', 'price', 'category', 'is_published']
         widgets = {
-            'is_published': forms.CheckboxInput(attrs={
-                'class': 'form-check-input',
-                'id': 'is_published'
-            }),
+            'is_published': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
             'description': forms.Textarea(attrs={'rows': 4}),
         }
         labels = {
             'is_published': 'Опубликовать товар на сайте',
         }
+
+
+    def clean_price(self):
+        price = self.cleaned_data['price']
+        if price <= 0:
+            raise ValidationError('Цена должна быть больше 0!')
+        return price
